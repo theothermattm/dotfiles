26350ddd5708724d989f75c785aa687ff103085e
Matt Merrill <matt.merrill@deptagency.com>
2025-08-12 14:48:01 -0400

    Some more neovim configs.

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,23 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    explorer = {
+      -- explorer things here
+    },
+    picker = {
+      sort = {
+        fields = { "modified" },
+      },
+      hidden = false,
+      ignored = true,
+      exclude = { ".DS_Store" },
+      sources = {
+        files = {
+          hidden = true,
+          files = { matcher = { frecency = true } },
+          buffers = { matcher = { frecency = true } },
+        },
+      },
+    },
+  },
+}
